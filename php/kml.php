d47c89aaca51f15f6f28fb37d434d674826c71ae
reubn <reuben@t3ch.tk>
2013-07-28 17:34:03 +0100

    TEMP

--- php/kml.php
+++
@@ -1,7 +0,0 @@
-<?php
-$points = $_GET['points'];
-$pointsa = str_replace("","", $points);
-$pointsb = explode("),", $pointsa);
-echo str_replace(")", "", str_replace("(", "", implode("<br />", $pointsb)));
-
-?>
